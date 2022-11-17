39c48661febf00e96849d64765c65933a3df922c
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-17 09:55:27 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Bilal Dallali on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
